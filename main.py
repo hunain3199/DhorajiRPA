7665a348e3c834dfade39c1f20f4d15a4d100862
Muhammad Hunain <92049246+hunain3199@users.noreply.github.com>
2022-01-17 18:19:31 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import scrapy
+
+class OlxSpider(scrapy.Spider):
+ name='olx'
+ start_urls = ['https://www.olx.com.pk/']
+
+ def parse(self, response):
+    all_url = response.xpath("//div[@class='_2de5fa66']/a/@href")
+    for i in all_url:
+        myurl = "https://www.olx.com.pk/" + i.extract()
+        yield scrapy.Request(url= myurl, callback=self.parse2)
+ def parse2(self, response):
+     prod_link = response.xpath("//div[@class='_44525bac']/a/@href")
+     for j in prod_link:
+         url_prod = "https://www.olx.com.pk/" + j.extract()
+         yield scrapy.Request(url=url_prod, callback=self.parse3)
+ def parse3(self, response):
+     all_product_link = response.xpath("//div[@class='ee2b0479']/a/@href")
+     for a in all_product_link:
+         all_link ="https://www.olx.com.pk/" + a.extract()
+         yield scrapy.Request(url=all_link, callback=self.parse_product)
+ def parse_product(self, response):
+     product_name = response.xpath("//h1[@class='a38b8112']/text()").get()
+     product_price = response.xpath("//span[@class='_56dab877']/text()").get()
+     yield{
+         'Product Name': product_name,
+         'Product Price': product_price
+     }
+
+
+
+
+
+
+
+
